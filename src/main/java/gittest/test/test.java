12225f755824f0cef4b49259091b7bc14dee4a21
KangK-666 <1285349543@qq.com>
2020-09-01 11:32:48 +0800

    add 02 by kk

---
+++ src/main/java/gittest/test/test.java
@@ -0,0 +1,12 @@
+package gittest.test;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hahaha");
+		System.out.println("agacsgd");
+
+	}
+
+}
